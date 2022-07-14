2e7215e8e7688da06e9e929e3c7074cbaee46fe1
Himal <himalkafle11@gmail.com>
2022-07-14 23:06:08 +0545

    profile-page

--- src/components/profile/ProfileMain.js
+++ src/components/profile/ProfileMain.js
@@ -6,6 +6,7 @@ import { profilePosts, friends } from "../../DummyData";
 import ProfileFeed from "../profile/ProfileFeed";
 import ProfileFriends from "./ProfileFriends";
 import Topbar from "../../components/topbar/Topbar";
+import Share from "../share/Share";
 
 function Profile() {
   const profile = profilePosts.map((profilePost) => {
@@ -18,20 +19,35 @@ function Profile() {
   return (
     <>
       <Topbar />
-      <div className="profileMainPage">
+      <div className="profilePage">
         <div className="profileSidebar">
           <Sidebar />
         </div>
 
-        <div className="profileMain">
-          <div className="profilePicCover"></div>
-          <div className="profileFeedFriends">
-            <div className="profileFeed">{profile}</div>
+        <div className="profileCoverPostFriends">
+          <div className="profilePicCover">
+            <img className="profileCover" src="../../assets/person/9.jpg" />
+            <img className="profilePhoto" src="../../assets/person/11.webp" />
+
+            <div className="profileText">
+              <h1 className="profileName">Himal Kafle</h1>
+              <h4 className="profileBio">Hello my friends !!!</h4>
+            </div>
+          </div>
+          <div className="profileMain">
+            <div className="profileFeedFriends">
+              <div className="shareFeed">
+                <div>
+                  <Share />
+                </div>
+                <div className="profileFeed">{profile}</div>
+              </div>
+            </div>
             <div className="friendsProfile">{user}</div>
           </div>
+
+          {/* <button onClick={() => navigate("register")}>Register</button> */}
         </div>
-        <h1></h1>
-        {/* <button onClick={() => navigate("register")}>Register</button> */}
       </div>
     </>
   );
