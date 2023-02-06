1ca415f393ff04f5a8d2039f39e227a81077611c
himalkafle11 <himalkafle11@gmail.com>
2023-02-06 23:15:13 +0545

    update

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -2,7 +2,7 @@ import Feedbar from "../../components/feedbar/Feedbar";
 import Topbar from "../../components/topbar/Topbar";
 import Sidebar from "../../components/sidebar/Sidebar";
 import Rightbar from "../../components/rightbar/Rightbar";
-import "./home.css";
+import "./Home.css";
 
 export default function Home() {
   return (
